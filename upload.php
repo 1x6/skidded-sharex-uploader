1513202f00d02ba37867e6a0b8a1dbc349085180
xeny <44981148+1x6@users.noreply.github.com>
2021-07-17 17:33:09 +0100

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,28 @@
+<?php
+$secret_key = ""; //Set this as your secret key, to prevent others uploading to your server.
+$sharexdir = ""; //This is your file dir, also the link..
+$domain_url = 'https://example.com/';
+$lengthofstring = 8; //Length of the file name
+
+function RandomString($length) {
+    $keys = array_merge(range(0,9), range('a', 'z'));
+
+    $key = '';
+    for($i=0; $i < $length; $i++) {
+        $key .= $keys[mt_rand(0, count($keys) - 1)];
+    }
+    return $key;
+}
+$filename = RandomString($lengthofstring);
+$target_file = $_FILES["sharex"]["name"];
+$fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType)) 
+{
+    echo $domain_url.$sharexdir.$filename.'.'.$fileType;
+}
+    else
+{
+   echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
+}
+?>
